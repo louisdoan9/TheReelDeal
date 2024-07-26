50e2ffba79a0bc4bdc1cccb59360efb54ec510e0
Louis Doan <louisdoan9@gmail.com>
2024-07-26 17:38:35 -0400

    Create placeholder for latest reviews section

---
+++ src/app/latest-reviews/latest-reviews.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-reviews',
+  standalone: true,
+  imports: [],
+  templateUrl: './latest-reviews.component.html',
+  styleUrl: './latest-reviews.component.scss'
+})
+export class LatestReviewsComponent {
+
+}

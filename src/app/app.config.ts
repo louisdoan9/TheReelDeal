7ca5d4fd93307ef30b3a20cae16871bb7de360f8
Louis Doan <louisdoan9@gmail.com>
2024-08-03 20:54:44 -0400

    Fix homepage CSS for smaller viewports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       routes,
-      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
     ),
   ],
 };
